29ac0d7a6fd8b54246f5ec39bf79f6d12dde35ff
David Dias <daviddias.p@gmail.com>
2015-09-30 23:42:34 +0100

    add versions command

---
+++ commands/versions.js
@@ -0,0 +1,24 @@
+var Command = require('ronin').Command
+var fs = require('fs')
+var path = require('path')
+
+module.exports = Command.extend({
+  desc: 'Check each version publishedPublish your project',
+
+  run: function (name) {
+    try {
+      var configPath = path.resolve(process.cwd() + '/ipsurge.json')
+      fs.statSync(configPath)
+      publish()
+    } catch (err) {
+      console.log('Project must be initiated first, run `ipsurge init`')
+    }
+
+    function publish () {
+      var config = JSON.parse(fs.readFileSync(configPath))
+      config.versions.forEach(function (version) {
+        console.log(version.timestamp, version.hash)
+      })
+    }
+  }
+})
