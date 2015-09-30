996142dfbc38901361594aa364d9d63f081e8d28
David Dias <daviddias.p@gmail.com>
2015-09-30 14:42:23 +0100

    add init command

--- commands/init.js
+++ commands/init.js
@@ -1,9 +1,31 @@
 var Command = require('ronin').Command
+var fs = require('fs')
+var ask = require('asking').ask
 
-var AppsAddCommand = module.exports = Command.extend({
+module.exports = Command.extend({
   desc: 'This command adds application',
 
   run: function (name) {
-    console.log('woo', name)
+    try {
+      fs.statSync(process.cwd() + '/ipsurge.json')
+      console.log('ipsurge was already initiated on this repo')
+    } catch (err) {
+      bootstrap()
+    }
+
+    function bootstrap () {
+      var config = {
+        versions: []
+      }
+      console.log('This utility will walk you through creating a ipsurge.json file.')
+      ask('Path of your Web Application (project)?', { default: 'public' }, function (err, path) {
+        if (err) {
+          return console.log(err) // TODO Handle this err properly
+        }
+        config['project'] = path
+        var fd = fs.openSync(process.cwd() + '/ipsurge.json', 'w')
+        fs.writeSync(fd, JSON.stringify(config, null, '  '), 0, 'utf-8')
+      })
+    }
   }
 })
